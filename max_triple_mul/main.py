1322727f6d0c91bef2b32dc87a9a57b06ffd7c1d
DenisDontsov <denis.dontsov@saritasa.com>
2018-03-06 14:35:26 +0700

    Create function max_triple_mul and tests for it

---
+++ max_triple_mul/main.py
@@ -0,0 +1,75 @@
+from bisect import bisect_left
+import operator
+from functools import reduce
+
+
+class ListSmallSize(Exception):
+    """ The list is smaller than the specified size """
+    pass
+
+
+def mul(lst: list) -> int:
+    """ Function to get product of list items.
+
+    Args:
+        lst (list): list of integers to multiply.
+
+    Returns:
+        int: multiplication result.
+    """
+    return reduce(operator.mul, lst, 1)
+
+
+def insert_to_sized_sorted_list(lst: list, x: int, max_size: int) -> bool:
+    """ Function to insert new value to sorted list with fixed size.
+    If size of lst less than max_size, or if x greatest then minimum
+    value in x, then insert x into lst. Else do nothing.
+    Important! Function edit input list
+
+    Args:
+        lst (list): list of integers.
+        x (int): value to insert.
+        max_size (int): max_size of input list
+
+    Returns:
+        bool: True, if the item was added
+    """
+    i = bisect_left(lst, x)
+    if i or len(lst) < max_size:
+        lst.insert(i, x)
+        return True
+
+
+def max_triple_mul(lst: list) -> int:
+    """ Function to find max production of 3 values in the list.
+
+    Args:
+        lst (list): list of integers to multiply (min. 3 items).
+
+    Returns:
+        int: max value of multiplication of 3 integers in the lst.
+
+    Raises:
+        ListSmallSize: Size of input list less than 3.
+    """
+
+    # check size of input list
+    if len(lst) < 3:
+        raise ListSmallSize("Size of list must be > 2")
+
+    # if list contains only 3 values
+    if len(lst) == 3:
+        return mul(lst)
+
+    # lists for 3 positive and 2 negative numbers
+    positive = []
+    negative = []
+
+    for x in lst:
+        # try insert each number to the appropriate list
+        if x >= 0:
+            insert_to_sized_sorted_list(positive, x, 3)
+        else:
+            insert_to_sized_sorted_list(negative, x, 2)
+
+    return max(mul(negative) * positive[-1], mul(positive[-3:]))
---
+++ max_triple_mul/test_main.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+from .main import max_triple_mul, ListSmallSize
+
+
+class TestMaxTripleProd(TestCase):
+    """ Test for function max_triple_mul """
+
+    def test_exception(self):
+        """ small sized lists """
+        with self.assertRaises(ListSmallSize):
+            max_triple_mul([1, 2])
+
+    def test_three_positive(self):
+        """ lists with only three positive items"""
+        result = max_triple_mul([1, 2, 3])
+        self.assertEqual(result, 6)
+
+        result = max_triple_mul([2, 3, 4])
+        self.assertEqual(result, 24)
+
+    def test_zero(self):
+        """ when function should return zero """
+        result = max_triple_mul([1, 2, 0])
+        self.assertEqual(result, 0)
+
+    def test_many_positive(self):
+        """ different test with positive and negative """
+
+        # pairs (input, result)
+        pairs = [
+            ([3, 4, 6, 8, 0], 192),
+            ([1, -1, 1, -1, 1], 1),
+            ([0, 0, 0, 0, -1], 0),
+            ([6, 5, 4, 3, 2], 120),
+            ([6, 5, 4, 3, -2], 120),
+            ([6, 5, 4, -3, -2], 120),
+            ([-6, -5, 4, 3, 0], 120),
+            ([-6, 5, 4, 3, 2], 60),
+            ([5, 6, -100, -1, 10], 1000)
+        ]
+
+        for lst, result in pairs:
+            self.assertEqual(max_triple_mul(lst), result)
